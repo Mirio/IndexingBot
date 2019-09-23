467348d86119a4e8d1a77cf2f5e8a346ae644dcf
mirio <miriodev@gmail.com>
2019-09-23 11:53:15 +0200

    Adding support to python3 and new deps

--- exportjson.py
+++ exportjson.py
@@ -4,34 +4,68 @@ import os
 import sys
 import json
 
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                             "./lib"))
 from algoliasearch import algoliasearch
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 
 # Init algolia
 algolia = algoliasearch.Client(algolia_id, algolia_secret)
 algolia_index = algolia.init_index("groups")
-res = algolia_index.search("", {"filters": 'enabled = 1', "hitsPerPage": 99999})
+res = algolia_index.search("", {"filters": "enabled = 1", "hitsPerPage": 99999})
 
+
+def telegram(url):
+    dcap = dict(DesiredCapabilities.PHANTOMJS)
+    dcap["phantomjs.page.settings.userAgent"] = (
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36"
+        " (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+    )
+    driver = webdriver.Remote(
+        command_executor="http://seleniumaddr:seleniumport", desired_capabilities=dcap
+    )
+    driver.get(url)
+    try:
+        WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "tgme_page_extra"))
+        )
+        req = driver.page_source
+        soup = BeautifulSoup(req, "lxml")
+        members = soup.find("div", {"class": "tgme_page_extra"})
+        members = members.text.replace(" members", "").replace(" ", "")
+    except:
+        members = 0
+    return members
+
+
+fout = open("error.log", "w")
 groups = {}
 for group in res["hits"]:
-    if group["category"] in groups:
-        groups[group["category"]].append(group)
+    if telegram(group["url"]) != 0:
+        if group["category"] in groups:
+            groups[group["category"]].append(group)
+        else:
+            groups[group["category"]] = [group]
     else:
-        groups[group["category"]] = [group]
-htmlout = ''
-htmlhead = ('<table class="table table-striped table-hover text-center">'
-            '<thead><tr><th>Nome Gruppo</th>'
-            '<th>Descrizione</th><th>Link</th></tr>'
-            '</thead><tbody>')
+        fout.write("[ERROR] %s" % group["name"])
+fout.close()
+htmlout = ""
+htmlhead = (
+    '<table class="table table-striped table-hover text-center">'
+    "<thead><tr><th>Nome Gruppo</th>"
+    "<th>Descrizione</th><th>Link</th></tr>"
+    "</thead><tbody>"
+)
 
-for category in groups:
-    htmlout += '<h2>%s</h2>%s' % (category, htmlhead)
+for category in sorted(groups):
+    htmlout += "<h2>%s</h2>%s" % (category, htmlhead)
     for group in groups[category]:
-        htmlout += ('<tr><td>%s</td><td>%s</td><td>'
-            '<a href="%s">Click</a></td></tr>') % (
-            group["name"], group["desc"],
-            group["url"])
-    htmlout += '</tbody></table>'
+        htmlout += (
+            "<tr><td>%s</td><td>%s</td><td>" '<a href="%s">Click</a></td></tr>'
+        ) % (group["name"], group["desc"], group["url"])
+    htmlout += "</tbody></table>"
 
-print htmlout.encode("utf-8")
+print(htmlout.encode("utf-8"))
--- indexingbot.py
+++ indexingbot.py
@@ -5,13 +5,12 @@ import logging
 from datetime import datetime
 from config import tgram_token, algolia_id, algolia_secret
 from time import sleep
-from urlparse import urlparse
+from urllib.parse import urlparse
 
 # Fix lambda dep
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                             "./lib"))
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "./lib"))
 import telebot
-from algoliasearch import algoliasearch
+from algoliasearch.search_client import SearchClient
 
 # Init telebot
 logger = telebot.logger
@@ -19,134 +18,168 @@ bot = telebot.TeleBot(tgram_token, threaded=False)
 telebot.logger.setLevel(logging.INFO)
 
 # Init algolia
-algolia = algoliasearch.Client(algolia_id, algolia_secret)
+algolia = SearchClient.create(algolia_id, algolia_secret)
+
 
 def cleantext(msg):
-    ''' Clear text from malicius data '''
+    """ Clear text from malicius data """
     return re.sub(r"[^a-z|A-Z|0-9|?| |'|!|?|_|-|,|.]", "", msg)
 
+
 def lambda_handler(event, context):
-    ''' Lambda Handler (webhook via api gateway) '''
+    """ Lambda Handler (webhook via api gateway) """
     update = telebot.types.Update.de_json(event["body"])
     bot.process_new_updates([update])
     return {
         "body": "ok",
         "statusCode": 200,
-        "headers": {"Content-Type": "application/json"}
+        "headers": {"Content-Type": "application/json"},
     }
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_handler(call):
-    ''' Handler for callbacks, run the function insert on call.data '''
+    """ Handler for callbacks, run the function insert on call.data """
     getattr(sys.modules[__name__], call.data)(call)
 
-@bot.message_handler(commands=['help', 'start'])
+
+@bot.message_handler(commands=["help", "start"])
 def start_helper(message):
-    ''' Helper for first command '''
+    """ Helper for first command """
     markup = telebot.types.InlineKeyboardMarkup()
     try:
         if "it" in message.from_user.language_code:
-            desc = ("Ciao %s, questo bot e' stato creato al fine di 'unificare'"
-                    "tutti i vari gruppi presenti nei network in un'unica "
-                    "directory (aggiornata e manutenuta) dove tutti gli "
-                    "utenti possono cercare i canali liberamente.\n"
-                    "Regole:\n- NO gruppi porno o simili.\n"
-                    "- NO gruppi che supportano Warez e qualsiasi "
-                    "altro illecito."
-                    "\n\nPer qualsiasi dubbi o domanda contatta @Mirioo\n"
-                    "Maggiori info al link https://indexingbot.github.io/ \n"
-                    "Lasciaci una review:"
-                    " https://storebot.me/bot/indexingbot" %
-                    message.from_user.first_name)
-            markup.add(telebot.types.InlineKeyboardButton(
-                "Richiedi l'aggiunta del tuo gruppo",
-                callback_data="add_helper"))
-            markup.add(telebot.types.InlineKeyboardButton(
-                "Guarda la lista dei gruppi", callback_data="showlist"))
-            markup.add(telebot.types.InlineKeyboardButton(
-                "Ricerca gruppo", callback_data="search"))
+            desc = (
+                "Ciao %s, questo bot e' stato creato al fine di 'unificare'"
+                "tutti i vari gruppi presenti nei network in un'unica "
+                "directory (aggiornata e manutenuta) dove tutti gli "
+                "utenti possono cercare i canali liberamente.\n"
+                "Regole:\n- NO gruppi porno o simili.\n"
+                "- NO gruppi che supportano Warez e qualsiasi "
+                "altro illecito."
+                "\n\nPer qualsiasi dubbi o domanda contatta @Mirioo\n"
+                "Maggiori info al link https://indexingbot.github.io/ \n"
+                "Lasciaci una review:"
+                " https://storebot.me/bot/indexingbot" % message.from_user.first_name
+            )
+            markup.add(
+                telebot.types.InlineKeyboardButton(
+                    "Richiedi l'aggiunta del tuo gruppo", callback_data="add_helper"
+                )
+            )
+            markup.add(
+                telebot.types.InlineKeyboardButton(
+                    "Guarda la lista dei gruppi", callback_data="showlist"
+                )
+            )
+            markup.add(
+                telebot.types.InlineKeyboardButton(
+                    "Ricerca gruppo", callback_data="search"
+                )
+            )
         else:
             raise TypeError
     except TypeError:
-        desc = ("Ciao %s, this bot was created with the purpose to index "
-                "all network groups in one single directory (maintained "
-                "and updated) where users can freely search.\nRules:\n"
-                "- No porno group or something like that.\n"
-                "- No Warez group or any illecit group.\n"
-                "If you have doubt please contact @Mirioo\n"
-                "More info: https://indexingbot.github.io/ \n"
-                "Review: https://storebot.me/bot/indexingbot\n" %
-                message.from_user.first_name)
-        markup.add(telebot.types.InlineKeyboardButton(
-            "Submit your group.", callback_data="add_helper"))
-        markup.add(telebot.types.InlineKeyboardButton(
-            "Show the group listed", callback_data="showlist"))
-        markup.add(telebot.types.InlineKeyboardButton(
-            "Search Group", callback_data="search"))
+        desc = (
+            "Ciao %s, this bot was created with the purpose to index "
+            "all network groups in one single directory (maintained "
+            "and updated) where users can freely search.\nRules:\n"
+            "- No porno group or something like that.\n"
+            "- No Warez group or any illecit group.\n"
+            "If you have doubt please contact @Mirioo\n"
+            "More info: https://indexingbot.github.io/ \n"
+            "Review: https://storebot.me/bot/indexingbot\n"
+            % message.from_user.first_name
+        )
+        markup.add(
+            telebot.types.InlineKeyboardButton(
+                "Submit your group.", callback_data="add_helper"
+            )
+        )
+        markup.add(
+            telebot.types.InlineKeyboardButton(
+                "Show the group listed", callback_data="showlist"
+            )
+        )
+        markup.add(
+            telebot.types.InlineKeyboardButton("Search Group", callback_data="search")
+        )
     bot.send_message(message.from_user.id, (desc), reply_markup=markup)
 
-@bot.message_handler(commands=['showlist'])
+
+@bot.message_handler(commands=["showlist"])
 def showlist(message):
-    ''' Show the group listed via algolia '''
+    """ Show the group listed via algolia """
     algolia_index = algolia.init_index("groups")
     msgout = ""
-    res = algolia_index.search(
-        "", {"filters": 'enabled = 1', "hitsPerPage": 500})
+    res = algolia_index.search("", {"filters": "enabled = 1", "hitsPerPage": 500})
     for group in res["hits"]:
         msgout += "--\nName: %s\nDescription: %s\nLink: %s\n" % (
-            group["name"], group["desc"], group["url"])
+            group["name"],
+            group["desc"],
+            group["url"],
+        )
 
     # Fix split large message
     splitted_text = telebot.util.split_string(msgout, 3000)
     for text in splitted_text:
-        bot.send_message(message.from_user.id, (text),
-                         disable_web_page_preview=True)
+        bot.send_message(message.from_user.id, (text), disable_web_page_preview=True)
 
-@bot.message_handler(commands=['waitlist'])
+
+@bot.message_handler(commands=["waitlist"])
 def waitlist(message):
-    ''' Show the group in wait list via algolia '''
+    """ Show the group in wait list via algolia """
     algolia_index = algolia.init_index("groups")
     msgout = ""
-    res = algolia_index.search(
-        "", {"filters": 'enabled = 0', "hitsPerPage": 500})
+    res = algolia_index.search("", {"filters": "enabled = 0", "hitsPerPage": 500})
     for group in res["hits"]:
         msgout += "--\nName: %s\nDescription: %s\nLink: %s\n" % (
-            group["name"], group["desc"], group["url"])
+            group["name"],
+            group["desc"],
+            group["url"],
+        )
 
     # Fix split large message
     splitted_text = telebot.util.split_string(msgout, 3000)
     for text in splitted_text:
-        bot.send_message(message.from_user.id, (text),
-                         disable_web_page_preview=True)
+        bot.send_message(message.from_user.id, (text), disable_web_page_preview=True)
 
-@bot.message_handler(commands=['search'])
+
+@bot.message_handler(commands=["search"])
 def search(message):
-    ''' Show the group listed via algolia '''
+    """ Show the group listed via algolia """
     try:
         msg_split = message.text.split("search ")
         if len(msg_split) <= 2:
             algolia_index = algolia.init_index("groups")
             msgout = ""
             res = algolia_index.search(
-                cleantext(msg_split[1]),
-                {"filters": 'enabled = 1', "hitsPerPage": 500})
+                cleantext(msg_split[1]), {"filters": "enabled = 1", "hitsPerPage": 500}
+            )
             for group in res["hits"]:
                 try:
                     if "it" in message.from_user.language_code:
                         msgout += "--\nNome: %s\nDescrizione: %s\nLink: %s\n" % (
-                            group["name"], group["desc"], group["url"])
+                            group["name"],
+                            group["desc"],
+                            group["url"],
+                        )
                     else:
                         raise TypeError
                 except TypeError:
                     msgout += "--\nName: %s\nDescription: %s\nLink: %s\n" % (
-                        group["name"], group["desc"], group["url"])
+                        group["name"],
+                        group["desc"],
+                        group["url"],
+                    )
             if not msgout:
                 msgout = "No group found."
             # Fix split large message
             splitted_text = telebot.util.split_string(msgout, 3000)
             for text in splitted_text:
-                bot.send_message(message.from_user.id, (text),
-                                 disable_web_page_preview=True)
+                bot.send_message(
+                    message.from_user.id, (text), disable_web_page_preview=True
+                )
             return
         else:
             raise KeyError
@@ -160,9 +193,10 @@ def search(message):
             msgout = "Use: /search <name to search>"
         bot.send_message(message.from_user.id, (msgout))
 
-@bot.message_handler(commands=['add'])
+
+@bot.message_handler(commands=["add"])
 def add_helper(message):
-    ''' Submit group to list '''
+    """ Submit group to list """
     try:
         algolia_index = algolia.init_index("groups")
         msg_split = message.text.split("add ")[1].split("|")
@@ -175,16 +209,18 @@ def add_helper(message):
             if not url.startswith("https://t.me/"):
                 try:
                     if "it" in message.from_user.language_code:
-                        bot.send_message(message.from_user.id,
-                                         ("Utilizza un url t.me valido."
-                                          " (usa https:// prima)"))
+                        bot.send_message(
+                            message.from_user.id,
+                            ("Utilizza un url t.me valido." " (usa https:// prima)"),
+                        )
                         return
                     else:
                         raise TypeError
                 except TypeError:
-                    bot.send_message(message.from_user.id,
-                                     ("Use the t.me format for URL."
-                                      " (use schema https)"))
+                    bot.send_message(
+                        message.from_user.id,
+                        ("Use the t.me format for URL." " (use schema https)"),
+                    )
                     return
 
             # Check if url is already present
@@ -193,16 +229,17 @@ def add_helper(message):
                 if group["url"] == url:
                     try:
                         if "it" in message.from_user.language_code:
-                            bot.send_message(message.from_user.id,
-                                             ("Gruppo gia' presente."))
+                            bot.send_message(
+                                message.from_user.id, ("Gruppo gia' presente.")
+                            )
                             return
                         else:
                             raise TypeError
                     except TypeError:
-                        bot.send_message(message.from_user.id,
-                                         ("Group already present."))
+                        bot.send_message(
+                            message.from_user.id, ("Group already present.")
+                        )
                         return
-
 
             # Check if name is already taken
             res = algolia_index.search(name)
@@ -210,29 +247,31 @@ def add_helper(message):
                 if group["name"] == name:
                     try:
                         if "it" in message.from_user.language_code:
-                            bot.send_message(message.from_user.id,
-                                             ("Nome gia' presente."))
+                            bot.send_message(
+                                message.from_user.id, ("Nome gia' presente.")
+                            )
                             return
                         else:
                             raise TypeError
                     except TypeError:
-                        bot.send_message(message.from_user.id,
-                                         ("Name already taken."))
+                        bot.send_message(message.from_user.id, ("Name already taken."))
                         return
 
             # Adding new items
             timenow = datetime.now().isoformat()
-            algolia_index.add_object({
-                "name": name,
-                "desc": desc,
-                "url": url,
-                "owner_id": message.from_user.id,
-                "owner_username": message.from_user.username,
-                "submit_date": timenow,
-                "update_date": timenow,
-                "category": "Main",
-                "enabled": False
-            })
+            algolia_index.add_object(
+                {
+                    "name": name,
+                    "desc": desc,
+                    "url": url,
+                    "owner_id": message.from_user.id,
+                    "owner_username": message.from_user.username,
+                    "submit_date": timenow,
+                    "update_date": timenow,
+                    "category": "Main",
+                    "enabled": False,
+                }
+            )
             try:
                 if "it" in message.from_user.language_code:
                     msgout = "%s correttamente aggiunto." % name
@@ -245,8 +284,7 @@ def add_helper(message):
     except (KeyError, IndexError, AttributeError):
         try:
             if "it" in message.from_user.language_code:
-                msgout = ("Usa: /add <url>|<Nome del gruppo>|"
-                          "<descrizione(max 100)>")
+                msgout = "Usa: /add <url>|<Nome del gruppo>|" "<descrizione(max 100)>"
             else:
                 raise TypeError
         except TypeError:
